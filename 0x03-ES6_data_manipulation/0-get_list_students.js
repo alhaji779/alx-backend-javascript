7e58a454333dfe2e290fa11e741d8af04f920d7e
Michael Kanu <michaelokanu01@yahoo.com>
2024-07-30 23:21:49 +0000

    update

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,11 +4,9 @@
  * @return {Array} A list of students
  */
 export default function getListStudents() {
-  const res = [
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-
-  return res;
-};
+}
--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,8 @@
-export default function getListStudentIds(n) {
-  if (!Array.isArray(n)) {
-    return [];
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    // Map the students to their IDs and return the result
+    return students.map((student) => student.id);
   }
-  else {
-    return n.map(x => x.id)
-  }
-};
+  // If students is not an array, return an empty array
+  return [];
+}
